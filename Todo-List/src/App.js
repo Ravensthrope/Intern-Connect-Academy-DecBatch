50e2c33c769ed0beb157250ca75a0ba6b018e773
Ravensthrope <95401791+Ravensthrope@users.noreply.github.com>
2024-01-07 11:56:07 +0530

    Add files via upload

---
+++ Todo-List/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Todo from "./Components/Todo";
+import Header from "./Components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Todo />
+      </div>
+    </>
+  );
+}
+
+export default App;
